69d9148755e6d850ae4ed6c9ccde496ae4ebd0d3
jrhbcn <jrhbcn@gmail.com>
2016-12-02 20:58:45 +0100

    Update homecontrol.py

--- homecontrol.py
+++ homecontrol.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 GPIO.setmode(GPIO.BOARD)
 
 # Create dictionary with elements 
-things = {'fans':   [{'name': 'bedroom',    'gpio': {'slow':11, 'stop':13, 'light':15, 'fast':16}}],
+things = {'fans':   [{'name': 'bedroom',    'gpio': {'slow':11,'stop':13, 'light':15, 'fast':16}}],
           'blinds': [{'name': 'bedroom',    'gpio': {'up':3,'stop':5,'down': 7}},
                      {'name': 'livingroom', 'gpio': {'up':8,'stop':10,'down':12}}]}
 
@@ -22,8 +22,9 @@ for k,val in things.items():
 
             
 def thing_do(f,action):
+    #print 'thing_do'
+    #print action 
     if action in f['gpio']:
-        #print 'thing_do' 
         GPIO.output(f['gpio'][action], GPIO.HIGH)
         time.sleep(1)
         GPIO.output(f['gpio'][action], GPIO.LOW)
