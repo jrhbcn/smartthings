09a83991043ccdb3f61c647e7b7b54a69dc64353
jrhbcn <jrhbcn@gmail.com>
2017-01-16 21:03:23 +0100

    Update bedroom fan pins for new wiring

--- scripts/homecontrol.py
+++ scripts/homecontrol.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 GPIO.setmode(GPIO.BOARD)
 
 # Create dictionary with elements 
-things = {'fans':   [{'name': 'bedroom',    'gpio': {'slow':11,'stop':13, 'light':15, 'fast':16}}],
+things = {'fans':   [{'name': 'bedroom',    'gpio': {'light':11, 'stop':13, 'fast':15, 'slow':16}}],
           'blinds': [{'name': 'bedroom',    'gpio': {'up':3,'stop':5,'down': 7}},
                      {'name': 'livingroom', 'gpio': {'up':8,'stop':10,'down':12}}]}
 
